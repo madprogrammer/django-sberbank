1c1de1d189f80501e003d207bc8641fcb51f6cf4
Sergey Anufrienko <serg@anufrienko.net>
2018-11-18 20:11:57 +0300

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='django-sberbank',
-    version='0.2.25',
+    version='0.2.26',
     description='Django app for Sberbank payments',
     url='http://github.com/madprogrammer/django-sberbank',
     author='Sergey Anufrienko',

05c56f324b10bb2fb7e10475b96d57ed1ea48af6
Lev Lybin <lev.lybin@gmail.com>
2019-09-30 19:33:14 +0700

    add install_requires in setup without hard versions, also excluded django-braces #3

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 
-setuptools.setup(name='django-sberbank',
+setuptools.setup(
+    name='django-sberbank',
     version='0.2.30',
     description='Django app for Sberbank payments',
     url='http://github.com/madprogrammer/django-sberbank',
@@ -10,10 +11,16 @@ setuptools.setup(name='django-sberbank',
     packages=setuptools.find_packages(),
     platforms='any',
     zip_safe=False,
+    install_requires=[
+        'django',
+        'django-jsonfield',
+        'djangorestframework',
+        'celery',
+        'requests',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ]
 )
-

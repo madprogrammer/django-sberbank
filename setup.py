b44ca8a218379af76beb3c96bbfaa954f4d8cd6e
Антон Маляров <anton.maliarov@fogstream.ru>
2020-01-14 15:10:54 +1000

    fix миграций

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='fs-django-sberbank',
-    version='0.2.34',
+    version='0.2.35',
     description='Django app for Sberbank payments',
     url='http://github.com/madprogrammer/django-sberbank',
     author='Sergey Anufrienko',
